0481f207906fa7d035adc7db44f19fdd8c4dad47
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-08-12 23:04:50 +0530

    Attempt 2A: 'GfG POTD Aug 10'
    Building up the solution

--- geeksforgeeks/lucky_alive.py
+++ geeksforgeeks/lucky_alive.py
@@ -2,21 +2,26 @@
     Original Problem: https://practice.geeksforgeeks.org/problems/lucky-alive-person-in-a-circle1229/1
 """
 
+
 # User function Template for python3
-import math
-
-
 class Solution:
     def find(self, N = 5):
         l = list(range(1, N + 1))
         sword = 0
+        killed = None
 
         while len(l) > 1:
-            sword = (sword + 1) % len(l)
+            # The person next to the one having sword is killed (in a circle)...
+            killed = (sword + 1) % len(l)
             try:
-                l.pop(sword)
+                # After the person is killed, it is removed from the queue...
+                l.pop(killed)
             except IndexError:
                 pass
+
+            # Since the killed person is removed, the next person in queue takes its position...
+            sword = killed
+
         return l[0]
 
 
@@ -26,7 +31,7 @@ class Solution:
 
 if __name__ == '__main__':
     ob = Solution()
-    print(ob.find(2000))
+    print(ob.find(90))
     # t = int(input())
     # for _ in range(t):
     #     N = int(input())
