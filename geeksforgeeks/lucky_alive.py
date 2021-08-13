3c055f7e3894d9b355948d7ad822b7764ba72a15
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-08-13 09:16:13 +0530

    Solved 'GfG POTD Aug 10'
    Discovered the 'trick'...
    Solved in time complexity O(log N)...

--- geeksforgeeks/lucky_alive.py
+++ geeksforgeeks/lucky_alive.py
@@ -5,45 +5,110 @@
 
 # User function Template for python3
 class Solution:
-    def next_alive(self, l: list, pos: int):
-        try:
-            p = l.index(1, pos + 1)
-        except ValueError:
-            p = l.index(1, 0)
+    def __init__(self):
+        # 10^8 is the max-limit of N. 10^8 would have around ~30 bits.
+        # Hence calculating pow(2)-LUT with sufficient margin.
+        self._powers_of_2 = [1 << n for n in range(35)]
 
-        return p
+    def floor_power_of_2(self, num):
+        """Find the nearest lower power of 2 from LUT
 
-    def find(self, N = 5):
-        l = [1] * N
-        sword = 0
-        killed = None
+           NOTE: We may use Binary Search here, however, since the LUT
+           is comparatively small in size, binsearch() won't have much
+           gain over linsearch()
+        """
+        if not num:
+            return 0
+        for i, pow in enumerate(self._powers_of_2):
+            if num < pow:
+                return self._powers_of_2[i - 1]
+            elif num == pow:
+                return pow
 
-        while True:
-            # The person next to the one having sword is killed (in a circle)...
-            killed = self.next_alive(l, sword)
+    def find(self, N=5):
+        """
+        :param N: Total number of persons in circle
+        :return: Index of person remaining (1-based)
 
-            if sword == killed:
-                break
+        :algorithm:
+        Watch the pattern of solutions:
+            N   find(N)
+            1  =  1
+            2  =  1
+            3  =  3
+            4  =  1
+            5  =  3
+            6  =  5
+            7  =  7
+            8  =  1
+            9  =  3
+            10  =  5
+            11  =  7
+            12  =  9
+            13  =  11
+            14  =  13
+            15  =  15
+            16  =  1
+            17  =  3
+            18  =  5
+            19  =  7
+            20  =  9
+            21  =  11
+            22  =  13
+            23  =  15
+            24  =  17
+            25  =  19
+            26  =  21
+            27  =  23
+            28  =  25
+            29  =  27
+            30  =  29
+            31  =  31
+            32  =  1
+            33  =  3
+            34  =  5
+            35  =  7
+            36  =  9
+            37  =  11
+            38  =  13
+            39  =  15
+            40  =  17
+            41  =  19
+            42  =  21
+            43  =  23
+            44  =  25
+            45  =  27
+            46  =  29
+            47  =  31
+            48  =  33
+            49  =  35
+            50  =  37
+            51  =  39
+            52  =  41
+            53  =  43
+            54  =  45
+            55  =  47
+            56  =  49
+            57  =  51
+            58  =  53
+            59  =  55
+            60  =  57
+            61  =  59
+            62  =  61
+            63  =  63
+        From the pattern, it can be seen that the answer is:
+            (N - floor_power_of_2(N) ) * 2 + 1
+        """
+        return ((N - self.floor_power_of_2(N)) << 1) + 1
 
-            # After the person is killed, it is removed from the queue...
-            l[killed] = 0
 
-            # Since the killed person is removed, the next person in queue takes its position...
-            sword = self.next_alive(l, killed)
-
-        return sword + 1
-
-
-# {
+#{
 #  Driver Code Starts
-# Initial Template for Python 3
-
+#Initial Template for Python 3
 if __name__ == '__main__':
-    ob = Solution()
-    print(ob.find(2000))
-    # t = int(input())
-    # for _ in range(t):
-    #     N = int(input())
-    #     ob = Solution()
-    #     print(ob.find(N))
+    t = int (input ())
+    for _ in range (t):
+        N = int(input())
+        ob = Solution()
+        print(ob.find(N))
 # } Driver Code Ends
