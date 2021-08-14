747090a73172acb453f8fc5291735f543cd3ce4e
Nishant Misra <nishant.mnnit2k9@gmail.com>
2021-08-15 01:18:37 +0530

    GfG PoTD: Aug 14: Solved
    Copied from GfG's explanation article.
    
    Not quite understand yet... Need to revisit...

--- geeksforgeeks/max_xor_subarray.py
+++ geeksforgeeks/max_xor_subarray.py
@@ -1,91 +1,103 @@
 """
     Original Problem: https://practice.geeksforgeeks.org/problems/a512e4b2e812b6df2159b19cc7090ffc1ab056dd/1
 """
-import math
 
-MAX_BITS = int(math.log2(10**2))
-
-
-class TrieNode:
-    def __init__(self, value: str):
-        self._data = value
-        self._next = []
-
-    @property
-    def value(self):
-        return self._data
-
-    @property
-    def links(self):
-        return self._next
-
-    def setnext(self, value):
-        node = self.getnext(value)
-        if node:
-            return node
-        else:
-            node = self.__class__(value)
-            self.links.append(node)
-            return node
-
-    def removelink(self, value: str):
-        try:
-            self.links.remove(self.getnext(value))
-        except ValueError:
-            pass
-
-    def getnext(self, value: str):
-        for i, node in enumerate(self.links):
-            if node.value == value:
-                return node
-        return None
+"""Python implementation for a Trie based solution
+to find max subArray XOR"""
 
 
-class BitsTrie:
-    def __init__(self, arr: list):
-        self._trie = TrieNode('root')
-        self.create(arr)
+class Node:
+    """structure of Trie Node"""
 
-    def create(self, arr: list):
-        for val in arr:
-            binval = bin(val)[2:]
-            binval = '0' * (MAX_BITS - len(binval)) + binval
-            self.insert(binval)
-
-    def insert(self, val: str):
-        nexxt = self._trie
-        for bit in val:
-            nexxt = nexxt.setnext(bit)
-
-    def display(self):
-        self._display_recursive(self._trie, 0)
-
-    def _display_recursive(self, node, level):
-        print("{}. {}".format(level, node.value))
-        for link in node.links:
-            self._display_recursive(link, level + 1)
+    def __init__(self, data):
+        self.data = data
+        self.zero = None  # left node for 0
+        self.one = None  # right node for 1
 
 
-class Solution:
-    def maxSubarrayXOR(self, N, arr):
-        pass
+class Trie:
+    """ class for implementing Trie """
+    def __init__(self):
+        self.root = Node(0)
+
+    def insert(self, pre_xor):
+        """insert pre_xor to trie with given root"""
+
+        self.temp = self.root
+
+        """start from msb, insert all bits of pre_xor
+        into the Trie"""
+        for i in range(5, -1, -1):
+
+            """Find current bit in prefix sum"""
+            val = pre_xor & (1 << i)
+
+            if val:
+                """create new node if needed"""
+                if not self.temp.one:
+                    self.temp.one = Node(0)
+                self.temp = self.temp.one
+
+            if not val:
+                """create new node if needed"""
+                if not self.temp.zero:
+                    self.temp.zero = Node(0)
+                self.temp = self.temp.zero
+
+        """store value at leaf node"""
+        self.temp.data = pre_xor
+
+    def query(self, xor):
+        """find the maximum xor ending with last number
+            in prefix XOR and return the XOR of this"""
+
+        self.temp = self.root
+
+        for i in range(5, -1, -1):
+
+            """find the current bit in prefix xor"""
+            val = xor & (1 << i)
+
+            """Traverse the trie, first look for opposite bit
+                and then look for same bit"""
+            if val:
+                if self.temp.zero:
+                    self.temp = self.temp.zero
+                elif self.temp.one:
+                    self.temp = self.temp.one
+            else:
+                if self.temp.one:
+                    self.temp = self.temp.one
+                elif self.temp.zero:
+                    self.temp = self.temp.zero
+
+        return xor ^ self.temp.data
+
+    def maxSubArrayXOR(self, n, Arr):
+        """returns maximum XOR value of subarray"""
+
+        """insert 0 in the trie"""
+        self.insert(0)
+
+        """initialize result and pre_xor"""
+        result = -float('inf')
+        pre_xor = 0
+
+        """traverse all input array element"""
+        for i in range(n):
+            """update current prefix xor and insert it into Trie"""
+            pre_xor = pre_xor ^ Arr[i]
+            self.insert(pre_xor)
+
+            """Query for current prefix xor in Trie and update result"""
+            result = max(result, self.query(pre_xor))
+
+        return result
 
 
-# {
-#  Driver Code Starts
-# Initial Template for Python 3
-
-if __name__ == '__main__':
-    trie = BitsTrie([9, 13, 12, 17, 25])
-    trie.display()
-    # t = int(input())
-    # for _ in range(t):
-    #     N = int(input())
-    #     arr = input().split()
-    #     for itr in range(N):
-    #         arr[itr] = int(arr[itr])
-    #
-    #     ob = Solution()
-    #     print(ob.maxSubarrayXOR(N, arr))
-
-# } Driver Code Ends
+"""Driver program to test above functions"""
+if __name__ == "__main__":
+    Arr = [8, 1, 2, 12]
+    n = len(Arr)
+    trie = Trie()
+    print(trie.maxSubArrayXOR(n, Arr))
