32d5432bbbc4550d563827d1775fdb2bb172b290
Nishant Misra <nishant.mnnit2k9@gmail.com>
2022-02-13 16:26:30 +0530

    LeetCode correct the non-recursive code for `reverse_string`

--- leetcode/reverse_string_2.py
+++ leetcode/reverse_string_2.py
@@ -14,11 +14,5 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        self.revs(s, 0)
-
-    def revs(self, s: List[str], n: int):
-        if n == len(s) // 2:
-            return
-
-        s[n], s[len(s) - 1 - n] = s[len(s) - 1 - n], s[n]
-        self.revs(s, n + 1)
+        for i in range(len(s) // 2):
+            s[i], s[-i - 1] = s[-i - 1], s[i]
